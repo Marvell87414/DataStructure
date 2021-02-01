59396aa92869171241ea3ee0c75f78c1e4489054
Marvell87414 <Marvell87414@users.noreply.github.com>
2021-02-02 00:25:45 +0800

    20210201
    LinkList(1) finsh

--- practice/practice/Source.cpp
+++ practice/practice/Source.cpp
@@ -1,41 +1,50 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<conio.h>
 int main() {
-	int i, j, NONZERO = 0;
-	int temp = 1;
-	int Sparse[6][6] = { 15,0,0,22,0,-15,0,11,3,0,0,0,0,0,0,-6,0,0,0,0,0,0,0,0,91,0,0,0,0,0,0,0,28,0,0,0 };
-	int Compress[9][3];
-	printf("[稀疏矩陣的各個元素\n]");
-	for ( i = 0; i < 6; i++)
+	int select, student_no = 0, num = 0;
+	float Msum = 0,Esum = 0;
+	struct student
 	{
-		for (j = 0;j < 6;j++) {
-			printf("[%d]\t ", Sparse[i][j]);
-			if (Sparse[i][j] != 0) NONZERO++;
+		char name[20];
+		int Math;
+		int Eng;
+		char no[10];
+		struct student *next;
+	};
+	typedef struct student s_data;
+	s_data *ptr;
+	s_data *head;
+	s_data *new_data;
+
+	head = (s_data*)malloc(sizeof(s_data));
+	head->next = NULL;
+	ptr = head;
+	do {
+		printf("(1)新增 (2)離開 =>");
+		scanf_s("%d", &select);
+		if (select != 2) {
+			printf("姓名 學號 數學成績 英文成績: ");
+			new_data = (s_data*)malloc(sizeof(s_data));
+			scanf_s("%s %s %d %d", new_data->name, new_data->no, &new_data->Math, &new_data->Eng);
+			ptr->next = new_data;
+			new_data->next = NULL;
+			ptr = ptr->next;
+			num++;
 		}
-		printf("\n");
-	}
-	Compress[0][0] = 6;
-	Compress[0][1] = 6;
-	Compress[0][2] = NONZERO;
-	for (i = 0; i < 6; i++)
+	} while (select != 2);
+	ptr = head->next;
+	putchar('\n');
+	while (ptr!=NULL)
 	{
-		for (j = 0;j < 6;j++) {
-			if (Sparse[i][j] != 0) {
-				Compress[temp][0] = i;
-				Compress[temp][1] = j;
-				Compress[temp][2] = Sparse[i][j];
-				temp++;
-			}
-		}
+		printf("姓名:%s\t 學號:%s\t 數學成績:%d\t 英文成績:%d\t\n", ptr->name, ptr->no, ptr->Math, ptr->Eng);
+		Msum += ptr->Math;
+		Esum += ptr->Eng;
+		student_no++;
+		ptr = ptr->next;
 	}
-	printf("[稀疏矩陣壓縮後的內容]\n");
-	for (i = 0;i < NONZERO;i++) {
-		for (j = 0; j < 3; j++)
-		{
-			printf("[%d] ", Compress[i][j]);
-		}
-		printf("\n");
-	}
+	printf("--------------------------------------------------\n");
+	printf("本串列學生數學平均成績:%.2f 英文平均成績%.2f\n",Msum/student_no,Esum/student_no);
+
 	system("pause");
+	return 0;
 }
